0a0648481dd08104ca7316061647645d87fda7bd
Sergei Volodin <etoestja@yandex.ru>
2017-02-23 22:52:15 +0300

    Upd

--- qualification/main.py
+++ qualification/main.py
@@ -69,10 +69,18 @@ def read_file(filename):
         sorted(BCList, key = lambda x: -x[1])
         BestCache[e] = BCList
 
+    global RH
     for r in range(R):
         [Rv, Re, Rn] = get_arr(inp)
+        heapq.heappush(RH, (goodness(Rv, Re, Rn), r, Rv, Re, Rn))
 
-
+def printState():
+    print("VERCX: " + str((V, E, R, C, X)))
+    print("VSizes: " + str(S))
+    print("CVE: " + str(CVE))
+    print("CachedVideos: " + str(CachedVideos))
+    print("RH: " + str(RH))
+    print("BestCache: " + str(BestCache))
 
 def processQ():
     req = heapq.heappop(RH)
@@ -88,15 +96,42 @@ def processQ():
             heapq.heappush(RH, (goodness(v, e, n), req[1], v, e, n))
 
 
+def write_answer(filename):
+    out = open(filename, 'w')
+    global CachedVideos
+    s = 0
+    for cv in CachedVideos:
+        cv = CachedVideos[cv]
+        if len(cv) > 0:
+            s += 1
+
+    out.write("{}\n".format(s))
+
+    for cv in CachedVideos:
+        idx = cv
+        cv = CachedVideos[cv]
+        if len(cv) > 0:
+            out.write("{} ".format(idx))
+            for x in cv:
+                out.write("{} ".format(x))
+            out.write("\n")
+
+def testVideos():
+    global CachedVideos
+    CachedVideos[0] = [1,2]
+    CachedVideos[1] = [3,4]
+
 def main():
     if len(sys.argv) < 3:
         sys.exit('Usage: %s input.in output.out'.format(sys.argv[0]))
     
 
     read_file(sys.argv[1])
-    print(V,E,R,C,X)
-    print(S)
-    out = open(sys.argv[2], 'w')
+
+
+    printState()
+    write_answer(sys.argv[2])
+
 
 if __name__ == '__main__':
     main()
